ec0406875e39b8e8bcc0a9f82dd78055b371d757
Sergio Nieto <sergionietojob@gmail.com>
2023-02-22 10:07:40 +0100

    Little main update

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "gc_stack.h"
 #include <stdio.h>
 
+
+/*In this program we will try to add 10 to one number. 
+ * Example: n = n + 10 */
 int main()
 {
   struct thread_info* tinfo = make_tinfo();
   
-  
+  /*We don't really know how to use the certicoq converted functions 
+   * to implement this add 10 program.*/
   
   long i = body(tinfo);
   printf("Body: %ld \n",i);
